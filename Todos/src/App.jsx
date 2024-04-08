81ebcb452aa569c58e3456a6ca7cef3c76bc2e25
anasnadkar45 <anasnadkar23@gmail.com>
2024-04-08 21:24:35 +0530

    Updating codebase

--- Todos/src/App.jsx
+++ Todos/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#0c1626] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
